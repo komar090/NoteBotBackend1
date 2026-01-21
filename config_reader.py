723db085f684149d352dd232ebffa6c610fac302
komar090 <91285128+komar090@users.noreply.github.com>
2026-01-21 22:10:13 +0700

    Add files via upload

--- config_reader.py
+++ config_reader.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     bot_token: SecretStr
     admin_ids: List[int]
     gigachat_auth: SecretStr
+    web_app_url: str = "https://komar090.github.io/NoteBotWeb/"
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', case_sensitive=False)
 
